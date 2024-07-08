1eb96b10888815f896f41663f3f4f0d7fa1d97fa
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-07-08 01:46:01 +0100

    updated

--- src/components/BottomNav/BottomNav.tsx
+++ src/components/BottomNav/BottomNav.tsx
@@ -8,19 +8,24 @@ const BottomNav = () => {
         <>
             <div className="bottomNav">
                 <div className="bottomContainer">
-                    <div>
-                        <IoMdHome className="bottomIcon"/>
-                        <NavLink to={"/"}>Home</NavLink>
-                    </div>
-                    <div>
-                        <GrNewWindow className="bottomIcon"/>
-                        <NavLink to={""}>New & Hot</NavLink>
-                    </div>
+                    <NavLink className="bottomNavTitle" to={"/"}>
+                        <div>
+                            <IoMdHome className="bottomIcon" />
+                            <p>Home</p>
+                        </div>
+                    </NavLink>
+                    <NavLink className="bottomNavTitle" to={""}>
+                        <div>
+                            <GrNewWindow className="bottomIcon" />
+                            <p>New & Hot</p>
+                        </div>
+                    </NavLink>
+                    <NavLink className="bottomNavTitle" to={""}>
                     <div>
                         <img className="profile-icon" src="https://occ-0-5543-299.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXYofKdCJceEP7pdxcEZ9wt80GsxEyXIbnG_QM8znksNz3JexvRbDLr0_AcNKr2SJtT-MLr1eCOA-e7xlDHsx4Jmmsi5HL8.png?r=1d4" alt=""></img>
-                        <NavLink to={""}>My Netflix</NavLink>
+                        <p>My Netflix</p>
                     </div>
-
+                    </NavLink>
 
                 </div>
             </div>
